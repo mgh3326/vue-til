3eec19fb117fae9931b511eefb8c979f0924e3b9
robin <mgh3326@naver.com>
2020-03-14 17:48:32 +0900

    라우터, 컴포넌트 설계

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,26 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import LoginPage from '@/views/LoginPage';
-import SignupPage from '@/views/SignupPage';
 
 Vue.use(VueRouter);
 
 export default new VueRouter({
+	mode: 'history',
 	routes: [
 		{
+			path: '/',
+			redirect: '/login',
+		},
+		{
 			path: '/login',
-			component: LoginPage,
+			component: () => import('@/views/LoginPage'),
 		},
 		{
 			path: '/signup',
-			component: SignupPage,
+			component: () => import('@/views/SignupPage'),
+		},
+		{
+			path: '*',
+			component: () => import('@/views/PageNotFound'),
 		},
 	],
 });
