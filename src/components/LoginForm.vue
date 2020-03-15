f9b74ca2f63bb76f3af0b04dad75e5f2438e9313
robin <mgh3326@naver.com>
2020-03-15 22:56:06 +0900

    브라우저 저장소를 이용한 인증 값 관리

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -29,9 +29,7 @@
 </template>
 
 <script>
-import { loginUser } from '@/api';
 import { validateEmail } from '@/utils/validation';
-
 export default {
 	data() {
 		return {
@@ -55,11 +53,8 @@ export default {
 					username: this.username,
 					password: this.password,
 				};
-				const { data } = await loginUser(userData);
-				console.log(data.token);
-				this.$store.commit('setToken', data.token);
-				this.$store.commit('setUsername', data.user.username);
-				this.$router.push('/main');
+				await this.$store.dispatch('LOGIN', userData);
+				await this.$router.push('/main');
 			} catch (error) {
 				// 에러 핸들링할 코드
 				console.log(error.response.data);
